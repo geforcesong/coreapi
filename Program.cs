7b4d4ca2aa2a54b7a5c83f1b103adea0a464fd35
gguo <gguo@movoto.com>
2018-10-29 14:39:56 -0700

    customized added port

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace coreapi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .UseStartup<Startup>().UseKestrel(options =>
+                   {
+                       options.Listen(System.Net.IPAddress.Loopback, 5333);
+                   });
     }
 }
